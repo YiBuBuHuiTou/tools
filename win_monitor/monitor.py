124065a0f96713234b456f98832f930bff914856
cxd <591018214@qq.com>
2022-12-26 23:23:14 +0800

    程序附带参数

--- win_monitor/monitor.py
+++ win_monitor/monitor.py
@@ -2,15 +2,24 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from controller.windows import MainWindow
-import pidfile
+from db import log
 
-# @pidfile
-def run():
-    app = QApplication(sys.argv)
-    mainWin = MainWindow()
-    mainWin.show()
-    sys.exit(app.exec_())
-
+LOGGER = log.LOGGER
 
 if __name__ == "__main__":
-    run()
+    
+    if len(sys.argv) <= 1:
+        LOGGER.debug("程序正常启动")
+        app = QApplication(sys.argv)
+        mainWin = MainWindow()
+        mainWin.show()
+        sys.exit(app.exec_())
+    # 后台运行
+    elif sys.argv[1] == "back":
+        LOGGER.debug("程序后台启动")
+        app = QApplication(sys.argv)
+        mainWin = MainWindow()
+        # mainWin.show()
+        sys.exit(app.exec_())
+    else:
+        LOGGER.error("程序启动参数不正确，启动失败")
